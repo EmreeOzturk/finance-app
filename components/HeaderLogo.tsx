23036628b05759b0f26a87436e97c762b3a16c6e
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-18 02:27:10 +0300

    refactor: Update HeaderLogo component with responsive flex classes

--- components/HeaderLogo.tsx
+++ components/HeaderLogo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const HeaderLogo = () => {
   return (
     <Link href="/">
-      <div className="items-center lg:flex hidden">
+      <div className="items-center md:flex hidden">
         <Image src="/logo.svg" alt="logo" height={28} width={28} />
         <p className="font-semibold text-zinc-300 text-2xl ml-2.5">Finance</p>
       </div>
