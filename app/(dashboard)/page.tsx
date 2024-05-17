3bfbc5255135fd9787d908a89905d494f2536bb4
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-17 10:59:37 +0300

    feat: Add UserButton component for signing out

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,9 @@
-
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
       <p>This is authenticated route</p>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
