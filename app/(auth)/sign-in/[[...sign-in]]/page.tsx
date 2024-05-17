fd314da76964ddc23e514e3e26bcb00561af184a
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-17 10:59:29 +0300

    refactor: Update sign-in page with Clerk's SignIn component and lazy loading for images

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,24 @@
-import { SignIn } from "@clerk/nextjs";
-
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import Image from "next/image";
+import { Loader } from "lucide-react";
 export default function Page() {
-  return <SignIn path="/sign-in" />;
+  return (
+    <div className="h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="flex flex-col items-center justify-center ">
+        <h1 className="font-bold text-3xl text-zinc-800">Welcome Back!</h1>
+        <p className="my-3 text-zinc-500">
+          Log in or create account to get back to your dashboard
+        </p>
+        <ClerkLoaded>
+          <SignIn path="/sign-in" routing="path" />
+        </ClerkLoaded>
+        <ClerkLoading>
+          <Loader size={32} className="animate-spin" />
+        </ClerkLoading>
+      </div>
+      <div className="hidden h-screen lg:flex bg-emerald-800 lg:justify-center lg:items-center">
+        <Image src={"/logo.svg"} alt="logo" width={250} height={250} />
+      </div>
+    </div>
+  );
 }
