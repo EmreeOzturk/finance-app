07912be716e724e5febd0e349b2ff29428e7c942
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-18 16:55:34 +0300

    Refactor user route to use new database schema

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,21 +1,9 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import userRoute from "./users";
-import { HTTPException } from "hono/http-exception";
 export const runtime = "edge";
 
-const app = new Hono()
-  .basePath("/api")
-  .route("/users", userRoute)
-  .onError((err, c) => {
-    if (err instanceof HTTPException) {
-      return err.getResponse();
-    }
-
-    return c.json({
-      error: "Internal Server Error",
-    }, 500);
-  });
+const app = new Hono().basePath("/api").route("/users", userRoute);
 
 export const GET = handle(app);
 export const POST = handle(app);
