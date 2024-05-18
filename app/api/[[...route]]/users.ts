fe63a75ffdd0597b85b9671c43964d25fb89ec2d
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-18 16:50:32 +0300

    feat: Update user route to fetch data for authenticated user

--- app/api/[[...route]]/users.ts
+++ app/api/[[...route]]/users.ts
@@ -3,6 +3,7 @@ import { db } from "@/db/drizzle";
 import { users } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { HTTPException } from "hono/http-exception";
+import { eq } from "drizzle-orm";
 const app = new Hono().get("/", clerkMiddleware(), async (c) => {
   const auth = getAuth(c);
   if (!auth?.userId) {
@@ -20,7 +21,8 @@ const app = new Hono().get("/", clerkMiddleware(), async (c) => {
       id: users.id,
       name: users.name,
     })
-    .from(users);
+    .from(users)
+    .where(eq(users.userId, auth.userId));
   return c.json({
     data,
   });
