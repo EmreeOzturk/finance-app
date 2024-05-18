e96f1498c39fc82fbd031f2368407c51466a7a35
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-18 16:28:21 +0300

    chore: Add QueryProvider to layout for managing API requests

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
-
+import QueryProvider from "./providers/QueryProvider";
 export const metadata: Metadata = {
   title: "Finance App",
   description: "A finance app built with Clerk and Hono/Bun.",
@@ -17,7 +17,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className="antialiased">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <QueryProvider>{children}</QueryProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
