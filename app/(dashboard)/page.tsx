eb116242a9113927dd4a77c3a8bd6c5cd42ab62a
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-18 18:26:22 +0300

    feat: Add button to dashboard page for creating a new user

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,18 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewUser } from "@/features/users/hooks/useNewUser";
 export default function Home() {
+  const { onOpen } = useNewUser();
   return (
     <div>
-      <p>This is authenticated route</p>
-      <UserButton afterSignOutUrl="/" />
+      <Button
+        onClick={() => {
+          onOpen();
+        }}
+      >
+        Add an user
+      </Button>
     </div>
   );
 }
