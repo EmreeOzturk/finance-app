2399d9b7874bb12f76c417fc73da3c14e0b51b81
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-18 15:52:42 +0300

    feat: Add users table to database schema

---
+++ db/schema.ts
@@ -0,0 +1,16 @@
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  uniqueIndex,
+  varchar,
+  primaryKey,
+  text,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  userId: text("user_id").notNull().unique(),
+});
