337156fc790b07f6906ba497bf9738c7b09761d5
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-18 17:59:55 +0300

    feat: Refactor user route to use zod-validator for input validation

--- app/api/[[...route]]/users.ts
+++ app/api/[[...route]]/users.ts
@@ -1,28 +1,45 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
-import { users } from "@/db/schema";
+import { insertUserSchema, users } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { eq } from "drizzle-orm";
-const app = new Hono().get("/", clerkMiddleware(), async (c) => {
-  const auth = getAuth(c);
-  if (!auth?.userId) {
-    return c.json(
-      {
-        error: "Unauthorized",
-      },
-      401
-    );
-  }
-  const data = await db
-    .select({
-      id: users.id,
-      name: users.name,
-    })
-    .from(users)
-    .where(eq(users.userId, auth.userId));
-  return c.json({
-    data,
+const app = new Hono()
+  .get("/", clerkMiddleware(), async (c) => {
+    const auth = getAuth(c);
+    if (!auth?.userId) {
+      return c.json(
+        {
+          error: "Unauthorized",
+        },
+        401
+      );
+    }
+    const data = await db
+      .select({
+        id: users.id,
+        name: users.name,
+      })
+      .from(users)
+      .where(eq(users.userId, auth.userId));
+    return c.json({
+      data,
+    });
+  })
+  .post("/", clerkMiddleware(), 
+    zValidator("json",insertUserSchema.pick({
+      name: true,
+    }))
+    ,async (c) => {
+    const auth = getAuth(c);
+    if (!auth?.userId) {
+      return c.json(
+        {
+          error: "Unauthorized",
+        },
+        401
+      );
+    }
   });
-});
 
 export default app;
