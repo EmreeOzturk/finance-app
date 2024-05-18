89eb6f20ca5ebb05c3c531105c15f4dc0940f1aa
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-18 16:47:41 +0300

    feat: Update Hono route error handling

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,11 +1,21 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
 import userRoute from "./users";
+import { HTTPException } from "hono/http-exception";
 export const runtime = "edge";
 
-const app = new Hono().basePath("/api").route("/users", userRoute);
+const app = new Hono()
+  .basePath("/api")
+  .route("/users", userRoute)
+  .onError((err, c) => {
+    if (err instanceof HTTPException) {
+      return err.getResponse();
+    }
+
+    return c.json({
+      error: "Internal Server Error",
+    }, 500);
+  });
 
 export const GET = handle(app);
 export const POST = handle(app);
