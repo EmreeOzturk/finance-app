fe7de7ec8bdd6c0386a1a09438c8cb27c2a6a73b
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-18 16:12:44 +0300

    feat: Add user route to handle API requests for user-related operations

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,13 +3,15 @@ import { handle } from "hono/vercel";
 import { z } from "zod";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { zValidator } from "@hono/zod-validator";
+import userRoute from "./users";
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-const routes = {};
+app.route("/users", userRoute);
+
 
 export const GET = handle(app);
 export const POST = handle(app);
 
-export type AppType = typeof routes;
+// export type AppType = typeof routes;
